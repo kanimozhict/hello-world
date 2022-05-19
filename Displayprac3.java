a1b030f205f2c7ea4e67494f47c09ee358d65472
kanimozhict <104317174+kanimozhict@users.noreply.github.com>
2022-05-19 15:49:22 -0400

    Add files via upload

---
+++ Displayprac3.java
@@ -0,0 +1,26 @@
+public class Displayprac3{
+  public static void main(String args[]){
+  /*  int a=10;
+    int b=2;
+    int c=a+b;
+    System.out.println(c);*/
+    /*String firstname="Aditi";
+    String lastname="Rajesh";
+    //System.out.println(firstname+lastname);
+    System.out.println(firstname+" "+lastname);*/
+    //space must be in double quotes
+    String name="Kani";
+    //System.out.println("Hello "+name);
+    //concat method
+    //String fullsentence="Welcome to the new era ".concat(name);
+    //System.out.println(fullsentence);
+    int a=90;
+    //String b=name+a;
+    //System.out.println(b);
+    /*int b=name+a;
+    this type if int is there before = then it will be of int type string cannot be into string */
+    System.out.println(name+a);
+
+  }//concat method
+
+}
